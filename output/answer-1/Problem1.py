ad501a63c901c9dead472be8164c20abf6ce0112
Pranesh1009 <91655682+Pranesh1009@users.noreply.github.com>
2021-09-30 13:39:21 +0530

    Create Problem1.py

---
+++ output/answer-1/Problem1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+Bungee = pd.read_csv('main.csv')
+Bungee.Year = pd.to_datetime(Bungee.Year, format = '%Y')
+Bungee.set_index('Year', drop=True, inplace=True)
+Bungee.drop(columns='Total', inplace=True)
+Bungees = Bungee.resample('10AS').sum()
+Bungees['Population'] = Bungee['Population'].resample('10AS').max()
+Bungees
