db1840f3dbc624ca9d6d2f303659b972bd1a8263
yojitha1289 <50201594+yojitha1289@users.noreply.github.com>
2021-10-02 12:55:55 +0530

    Create answer3.py

---
+++ output/answer-3/answer3.py
@@ -0,0 +1,6 @@
+#Ques_3
+data =pd.read_csv("intership_test2/input/question-3/main.csv")
+d1 = {'Occupation':df['occupation'],'Age':df['age']}
+df = pd.DataFrame(d1)
+result = df.groupby('Occupation').agg({'age': ['min', 'max']})
+print(result)
